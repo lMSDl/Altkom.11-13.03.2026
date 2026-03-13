1ce51da87192fa7cecbb93b317ebfbc9b73bfe92
Paweł <pawel@msd.info.pl>
2026-03-13 09:54:21 +0100

    WebSocket controller

--- WebSockets/Program.cs
+++ WebSockets/Program.cs
@@ -2,66 +2,14 @@ using System.Net.WebSockets;
 using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
 var app = builder.Build();
 
 app.UseWebSockets(new WebSocketOptions { KeepAliveInterval = TimeSpan.FromSeconds(30)});
 
-app.MapWhen(context => context.WebSockets.IsWebSocketRequest, appSocket =>
-{
-    appSocket.Run(async context =>
-    {
-        var webSocket = await context.WebSockets.AcceptWebSocketAsync();
-
-        var helloMessage = "Hello WebSocket!";
-
-        {
-
-            var helloBytes = System.Text.Encoding.UTF8.GetBytes(helloMessage);
-            await webSocket.SendAsync(
-                //new ArraySegment<byte>(helloBytes),
-                new ArraySegment<byte>(helloBytes, 0, 5),
-                System.Net.WebSockets.WebSocketMessageType.Text,
-                false, //flaga oznaczająca, że to nie jest ostatnia część wiadomości
-                CancellationToken.None);
-            await webSocket.SendAsync(
-                //new ArraySegment<byte>(helloBytes),
-                new ArraySegment<byte>(helloBytes, 5, helloBytes.Length - 5),
-                System.Net.WebSockets.WebSocketMessageType.Text,
-                true, //flaga oznaczająca, że to jest ostatnia część wiadomości
-                CancellationToken.None);
-        }
-
-        var buffer = new byte[1024];
-        WebSocketReceiveResult receiveResult;
-        StringBuilder stringBuilder = new();
-        do
-        {
-            stringBuilder.Clear();
-            do {
-                receiveResult = await webSocket.ReceiveAsync(buffer, CancellationToken.None);
-                stringBuilder.Append(Encoding.UTF8.GetString(buffer, 0, receiveResult.Count));
-            } while (!receiveResult.EndOfMessage);
-
-            await webSocket.SendAsync(
-                new ArraySegment<byte>(Encoding.UTF8.GetBytes(stringBuilder.ToString())),
-                WebSocketMessageType.Text,
-                true,
-                CancellationToken.None);
-
-            /*var message = Encoding.UTF8.GetString(buffer, 0, receiveResult.Count);
-            Console.WriteLine(message);
-
-            await webSocket.SendAsync(
-                new ArraySegment<byte>(buffer, 0, receiveResult.Count),
-                receiveResult.MessageType,
-                receiveResult.EndOfMessage,
-                CancellationToken.None);*/
-
-        } while (!receiveResult.CloseStatus.HasValue);
-
-    });
-});
-
 app.MapGet("/", () => "Hello World!");
+
+app.MapControllers();
 
 app.Run();
